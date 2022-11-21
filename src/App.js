66fb76abe6ca4c4f5c1bb95281aab543561d8ffa
juan-suarez <jsuarez0698@gmail.com>
2022-11-21 12:54:50 -0400

    starting with styles and first hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
 
 function App() {
+
+  //const API_KEY = "fcf989ca6812a03a6612f1f228806100";
+  //const API_BASE_URL = 'http://api.openweathermap.org/';
+  //const link = "api.openweathermap.org/data/2.5/forecast?q=London,us&appid={API_KEY}";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. here
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className='app-container'>
+      <div className="App">
+        <h1>Weather app</h1>
+        <div className='overlay'>
+          <div className='container'>
+            <div className='input'>
+              <input type="text" name="city" placeholder="entername"/>
+              <button>temeprature</button>
+            </div>
+            <div className='temperature-results'>
+              <h3>City, country</h3>
+              <img src="descarga.jpg" alt='background'/>
+              <h3>normal weather</h3>
+            </div>
+            <div className='degree'>
+              <h1>32 C</h1>
+            </div>
+          </div>
+        </div>
     </div>
+    </section>
   );
 }
 
